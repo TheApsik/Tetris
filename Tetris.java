bebb6468b66128d98f35d63f3725a745e51c5d92
TheApsik <apsiiiik@gmail.com>
2016-12-06 11:44:10 +0100

    Add files via upload

---
+++ Tetris.java
@@ -0,0 +1,11 @@
+/**
+ * Created by uczen on 06.12.2016.
+ */
+public class Tetris {
+    public static void main(String... arg){
+        Block.Figure a = Block.Figure.Z;
+        a.getWalue();
+        a.rotate();
+        a.getWalue();
+    }
+}
